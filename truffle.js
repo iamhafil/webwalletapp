b35b5b542f701a132219795bca7822e573c7a8ae
Muhammed Hafil <hafizc007@gmail.com>
2018-08-06 01:03:05 +0530

    added support for ropsten network

--- truffle.js
+++ truffle.js
@@ -1,32 +1,32 @@
-module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // for more about customizing your Truffle configuration!
-  networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
-    }
-  }
-};
-
-//
 // module.exports = {
+//   // See <http://truffleframework.com/docs/advanced/configuration>
+//   // for more about customizing your Truffle configuration!
 //   networks: {
 //     development: {
-//       host: "localhost",
-//       port: 8545,
+//       host: "127.0.0.1",
+//       port: 7545,
 //       network_id: "*" // Match any network id
-//     },
-//      ropsten:  {
-//      network_id: 3,
-//      host: "localhost",
-//      port:  8545,
-//      gas:   2900000
-// }
-//   },
-//    rpc: {
-//  host: 'localhost',
-//  post:8080
-//    }
+//     }
+//   }
 // };
+
+
+module.exports = {
+   networks:{
+      development:{
+         host:"127.0.0.1",
+         port:7545,
+         network_id:"*" // Match any network id
+      },
+      ropsten:{
+         network_id:3,
+         host:"localhost",
+         port:8545,
+         gas:2900000
+      }
+   },
+   rpc:{
+      host:'localhost',
+      post:8080
+   }
+};
